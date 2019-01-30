54f9f4e232cb1dc597f607f428149c65fa39d174
joao <joao182cx@gmail.com>
2019-01-30 08:17:26 -0200

    Classe CirculoPanel que irá desenhar o círculo.

--- src/CirculoPanel.java
+++ src/CirculoPanel.java
@@ -1,7 +1,42 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
 import javax.swing.JPanel;
 
 public class CirculoPanel extends JPanel{
 	
+	private final Circulo circ;
+	private final int eixoX;
+	private final int eixoY;
 	
+	public CirculoPanel(double raio, int eixoX, int eixoY) {
+		super();
+		this.circ = new Circulo(raio);
+		this.eixoX = eixoX;
+		this.eixoY = eixoY;
+	}
 
+	public Circulo getCirc() {
+		return circ;
+	}
+
+	public int getEixoX() {
+		return eixoX;
+	}
+
+	public int getEixoY() {
+		return eixoY;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.draw(new Ellipse2D.Double(getEixoX(), getEixoY(), circ.getDiametro(), circ.getDiametro()));
+		
+	}
+	
 }
