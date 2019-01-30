c643895e6a7fdfc0a264665bb598456df37823c0
joao <joao182cx@gmail.com>
2019-01-30 07:53:07 -0200

    Layout do app que receberá os dados para desenhar o círculo.

---
+++ src/CirculoApp.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class CirculoApp {
+	
+	public static void main(String[] args) {
+		
+		CirculoFrame circ = new CirculoFrame();
+		circ.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		circ.setSize(1000, 1000);
+		circ.setVisible(true);
+		
+	}
+
+}
---
+++ src/CirculoFrame.java
@@ -0,0 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class CirculoFrame extends JFrame {
+	
+	private final JPanel panelControle; 
+
+	private final JLabel labelRaio;
+	private final JLabel labelEixoX;
+	private final JLabel labelEixoY;
+	
+	private final JTextField fieldRaio;
+	private final JTextField fieldEixoX;
+	private final JTextField fieldEixoY;
+	
+	private final JButton buttonCalcular;
+	
+	public CirculoFrame() {
+		
+		super("Circulo");
+		
+		panelControle = new JPanel(new FlowLayout());
+		
+		labelRaio = new JLabel("Raio: ");
+		labelEixoX = new JLabel("Eixo X: ");
+		labelEixoY = new JLabel("Eixo Y: ");
+		
+		fieldRaio = new JTextField(5);
+		fieldEixoX =  new JTextField(5);
+		fieldEixoY =  new JTextField(5);
+		
+		buttonCalcular = new JButton("Calcular");
+		
+		panelControle.add(labelRaio);
+		panelControle.add(fieldRaio);
+		
+		panelControle.add(labelEixoX);
+		panelControle.add(fieldEixoX);
+		
+		panelControle.add(labelEixoY);
+		panelControle.add(fieldEixoY);
+		
+		panelControle.add(buttonCalcular);
+		
+		add(panelControle, BorderLayout.NORTH);
+		
+	}
+	
+	
+}
